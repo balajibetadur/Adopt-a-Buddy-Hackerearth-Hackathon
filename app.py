7ee671be6f153cb51babe4d5597524ab2b93aadd
balajibetadur <balajibetadur@gmail.com>
2020-09-15 19:28:41 +0530

    finally

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask,render_template,request
-app = Flask(__name__)
 import pickle
+
+
+app = Flask(__name__)
+
 # from sklearn.externals import joblib
 model = pickle.load(open('model.pkl','rb'))
 
@@ -9,14 +12,20 @@ def pred():
     if request.method == 'POST':
         # try:
             params = request.form
+
+
             duration = int(params['duration'])
             color = int(params['color'])
             breed = float(params['breed'])
             x1 = int(params['x1'] )  
             x2 = int(params['x2'])
             condition = int(params['condition'])
-            length, height = 0, 0
-            result = model.predict([[breed,length, height, duration, color, x1, x2, condition]])
+            height = int(params['height'])
+            length = float(params['width'])
+
+
+            # length, height = 0, 0
+            result = model.predict([[condition,color, length, height,  x1, x2,breed,duration]])
             return render_template('result.html',result = result)
         # except:
         #     return render_template('index.html')
